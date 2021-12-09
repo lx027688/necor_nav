6636b14328e053a5c20924002ca31c8cce073485
lixiang <027688lx>
2021-12-09 23:31:37 +0800

    添加影视资源网站

--- themes/WebStack-Hugo/static/assets/js/onenav-app.js
+++ themes/WebStack-Hugo/static/assets/js/onenav-app.js
@@ -1290,7 +1290,7 @@ function ioConfirm(message, btnCallBack) {
 	}
 	return popup;
 }
-console.log("\n %c One Nav 导航主题 By 一为 %c https://www.iotheme.cn/ \n", "color: #ffffff; background: #f1404b; padding:5px 0;", "background: #030307; padding:5px 0;");
+// console.log("\n %c One Nav 导航主题 By 一为 %c https://www.iotheme.cn/ \n", "color: #ffffff; background: #f1404b; padding:5px 0;", "background: #030307; padding:5px 0;");
 
 /**
  * Minified by jsDelivr using Terser v5.3.5.
